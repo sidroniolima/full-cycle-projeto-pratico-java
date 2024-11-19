ce5d3c702928c88664950f29dde63dff14d55b40
Sidronio Lima <sidronio@sidroniolima.com.br>
2024-11-19 11:59:21 -0300

    UpdateMediaStatusUseCaseTest

--- fc3-admin-do-catalogo/application/src/test/java/br/com/sidroniolima/admin/application/video/media/update/UpdateMediaStatusUseCaseTest.java
+++ fc3-admin-do-catalogo/application/src/test/java/br/com/sidroniolima/admin/application/video/media/update/UpdateMediaStatusUseCaseTest.java
@@ -1,4 +1,167 @@
 package br.com.sidroniolima.admin.application.video.media.update;
 
-public class UpdateMediaStatusUseCaseTest {
+import br.com.sidroniolima.admin.application.UseCaseTest;
+import br.com.sidroniolima.admin.domain.Fixture;
+import br.com.sidroniolima.admin.domain.video.MediaStatus;
+import br.com.sidroniolima.admin.domain.video.Video;
+import br.com.sidroniolima.admin.domain.video.VideoGateway;
+import br.com.sidroniolima.admin.domain.video.VideoMediaType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Mockito.*;
+
+public class UpdateMediaStatusUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultUpdateMediaUseCase useCase;
+
+    @Mock
+    private VideoGateway videoGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(videoGateway);
+    }
+
+    @Test
+    public void givenCmdForVideo_whenIsValid_shouldUpdateStatusAndEncodedLocation() {
+        // given
+        final var expectedStatus = MediaStatus.COMPLETED;
+        final var expectedFolder = "encoded_media";
+        final var expectedFileName = "filename.mp4";
+        final var expectedType = VideoMediaType.VIDEO;
+        final var expectedMedia = Fixture.Videos.audioVideo(expectedType);
+
+        final var aVideo = Fixture.Videos.systemDesign()
+                .setVideo(expectedMedia);
+
+        final var expectedId = aVideo.getId();
+
+        when(videoGateway.findById(any()))
+                .thenReturn(Optional.of(aVideo));
+
+        when(videoGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var aCmd = UpdateMediaCommand.with(
+                expectedStatus,
+                expectedId.getValue(),
+                expectedMedia.id(),
+                expectedFolder,
+                expectedFileName
+        );
+
+        // when
+        this.useCase.execute(aCmd);
+
+        // then
+        final var captor = ArgumentCaptor.forClass(Video.class);
+
+        verify(videoGateway, times(1)).findById(eq(expectedId));
+        verify(videoGateway, times(1)).update(captor.capture());
+
+        final var actualVideo = captor.getValue();
+
+        Assertions.assertTrue(aVideo.getTrailer().isEmpty());
+
+        final var actualVideoMedia = actualVideo.getVideo().get();
+
+        Assertions.assertEquals(expectedMedia.id(), actualVideoMedia.id());
+        Assertions.assertEquals(expectedMedia.rawLocation(), actualVideoMedia.rawLocation());
+        Assertions.assertEquals(expectedMedia.checksum(), actualVideoMedia.checksum());
+        Assertions.assertEquals(expectedStatus, actualVideoMedia.status());
+        Assertions.assertEquals(expectedFolder.concat("/").concat(expectedFileName), actualVideoMedia.encodedLocation());
+    }
+
+    @Test
+    public void givenCmdForTrailer_whenIsValid_shouldUpdateStatusAndEncodedLocation() {
+        // given
+        final var expectedStatus = MediaStatus.COMPLETED;
+        final var expectedFolder = "encoded_media";
+        final var expectedFileName = "filename.mp4";
+        final var expectedType = VideoMediaType.TRAILER;
+        final var expectedMedia = Fixture.Videos.audioVideo(expectedType);
+
+        final var aVideo = Fixture.Videos.systemDesign()
+                .setVideo(expectedMedia);
+
+        final var expectedId = aVideo.getId();
+
+        when(videoGateway.findById(any()))
+                .thenReturn(Optional.of(aVideo));
+
+        when(videoGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var aCmd = UpdateMediaCommand.with(
+                expectedStatus,
+                expectedId.getValue(),
+                expectedMedia.id(),
+                expectedFolder,
+                expectedFileName
+        );
+
+        // when
+        this.useCase.execute(aCmd);
+
+        // then
+        final var captor = ArgumentCaptor.forClass(Video.class);
+
+        verify(videoGateway, times(1)).findById(eq(expectedId));
+        verify(videoGateway, times(1)).update(captor.capture());
+
+        final var actualVideo = captor.getValue();
+
+        Assertions.assertTrue(aVideo.getVideo().isEmpty());
+
+        final var actualVideoMedia = actualVideo.getTrailer().get();
+
+        Assertions.assertEquals(expectedMedia.id(), actualVideoMedia.id());
+        Assertions.assertEquals(expectedMedia.rawLocation(), actualVideoMedia.rawLocation());
+        Assertions.assertEquals(expectedMedia.checksum(), actualVideoMedia.checksum());
+        Assertions.assertEquals(expectedStatus, actualVideoMedia.status());
+        Assertions.assertEquals(expectedFolder.concat("/").concat(expectedFileName), actualVideoMedia.encodedLocation());
+    }
+
+    @Test
+    public void givenCmdForTrailer_whenIsInvalid_shouldDoNothing() {
+        // given
+        final var expectedStatus = MediaStatus.COMPLETED;
+        final var expectedFolder = "encoded_media";
+        final var expectedFileName = "filename.mp4";
+        final var expectedType = VideoMediaType.TRAILER;
+        final var expectedMedia = Fixture.Videos.audioVideo(expectedType);
+
+        final var aVideo = Fixture.Videos.systemDesign()
+                .setVideo(expectedMedia);
+
+        final var expectedId = aVideo.getId();
+
+        when(videoGateway.findById(any()))
+                .thenReturn(Optional.of(aVideo));
+
+        final var aCmd = UpdateMediaCommand.with(
+                expectedStatus,
+                expectedId.getValue(),
+                "random_id",
+                expectedFolder,
+                expectedFileName
+        );
+
+        // when
+        this.useCase.execute(aCmd);
+
+        // then
+        verify(videoGateway, times(1)).findById(eq(expectedId));
+        verify(videoGateway, times(0)).update(any());
+    }
 }
